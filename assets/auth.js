873a16def3662cf7b167d415af829b36b9862454
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-11-19 04:20:45 +0100

    Add files via upload

--- assets/auth.js
+++ assets/auth.js
@@ -1,7 +1,6 @@
 function login(){
-  const client_id="1440533345523204217";
+  const client_id="DEINE_CLIENT_ID";
   const redirect="https://engels811.github.io/wonderlife-dashboard/index.html";
   const url=`https://discord.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect)}&response_type=token&scope=identify%20guilds%20guilds.members.read`;
   window.location.href=url;
 }
-function logout(){localStorage.removeItem("token");window.location.href="index.html";}

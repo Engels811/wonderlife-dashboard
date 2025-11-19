ceb1c92df95575f01542b6123a182121549003b2
Engels811 <54458787+Engels811@users.noreply.github.com>
2025-11-19 04:14:48 +0100

    Update auth.js

--- assets/auth.js
+++ assets/auth.js
@@ -1,5 +1,5 @@
 function login(){
-  const client_id="DEINE_CLIENT_ID";
+  const client_id="1440533345523204217";
   const redirect="https://engels811.github.io/wonderlife-dashboard/index.html";
   const url=`https://discord.com/oauth2/authorize?client_id=${client_id}&redirect_uri=${encodeURIComponent(redirect)}&response_type=token&scope=identify%20guilds%20guilds.members.read`;
   window.location.href=url;
